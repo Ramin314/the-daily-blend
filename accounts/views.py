46f53d817368a25a59579e80da19190c436f5979
Ramin314 <ramintawab@hotmail.co.uk>
2023-12-04 01:40:59 +0000

    fix: logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import redirect, render
 
-# Create your views here.
+def custom_logout(request):
+    logout(request)
+    return redirect('index')
--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import custom_logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/logout/', custom_logout, name='account_logout'),  # Override the allauth logout URL
     path('accounts/', include('allauth.urls')),
     path('', include('app.urls')),
 ]
